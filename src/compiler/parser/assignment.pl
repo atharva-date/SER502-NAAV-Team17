44bf712250d188bb3a7933500debb2ba6ae2a182
Nisha Verma <nverma20@asu.edu>
2024-04-25 12:02:27 -0700

    Improved assignment

--- src/compiler/parser/assignment.pl
+++ src/compiler/parser/assignment.pl
@@ -1,23 +1,46 @@
 % author; purpose; version; date
 % Nisha Verma; Parser for assignment; 1.0; 04/20/2024
-
+% Nisha Verma; Improvised assignment; 2.0; 04/25/2024
 :- use_rendering(svgtree).
 
 program(Parse) --> main_method(Parse).
+
 main_method(main_block(Block)) --> ["main"], block(Block).
-block(block(Statements)) --> ["{"], statement_list(Statements), ["}"].
-statement_list([Statement|Rest]) --> statement(Statement), statement_list(Rest).
-statement_list([]) --> [].
-statement(assignment(Identifier, Value)) --> identifier(Identifier), ["="], value(Value), [";"].
-identifier(identifier(Id)) --> [Id], { atom_chars(Id, [X|Rest]), char_type(X, alpha), maplist(char_type, Rest, [alnum|_]) }.
-value(boolean_value(true)) --> ["T"].
-value(boolean_value(false)) --> ["F"].
-value(numeric_value(Int)) --> integer(Int).
-value(string_value(String)) --> string_value(String).
-integer(Int) --> digit_sequence(Seq), { number_chars(Int, Seq) }.
-digit_sequence([Digit|Rest]) --> digit(Digit), digit_sequence(Rest).
-digit_sequence([Digit]) --> digit(Digit).
-digit(D) --> [D], { char_type(D, digit) }.
-string_value(String) --> "\"", string_characters(String), "\"".
-string_characters([Char|Rest]) --> [Char], { dif(Char, "\"") }, string_characters(Rest).
-string_characters([]) --> [].
+block(block(Statements)) -->  ["{"], statement_list(Statements), ["}"].
+statement_list(sl(S,R)) --> statement(S),statement_list(R).
+statement_list(sl(S))--> statement(S).
+statement(st(P)) --> print_statement(P), [";"].
+statement(assignment(Identifier, Value)) --> id(Identifier), ["="], value(Value), [";"].
+
+value(number(X)) --> [X], { number(X) }.
+value(string(V)) --> string_value(V).
+value(identifier(X)) --> id(identifier(X)).
+
+value(boolean('T')) --> [T], { member(T, ['T']) }.
+value(boolean('F')) --> [F], { member(F, ['F']) }.
+numeric_value(I) --> integer(I).
+numeric_value(F) --> float(F).
+
+id(identifier(X)) --> [X],{ member(X, [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]) }.
+
+integer(int(I)) --> digit_sequence(I).
+float(float(I,D)) --> digit_sequence(I), [“.”], digit_sequence(D).
+digit_sequence((I,D)) --> digit(I), digit_sequence(D).
+digit_sequence(D) --> digit(D).
+digit(0) --> "0".
+digit(1) --> "1".
+digit(2) --> "2".
+digit(3) --> "3".
+digit(4) --> "4".
+digit(5) --> "5".
+digit(6) --> "6".
+digit(7) --> "7".
+digit(8) --> "8".
+digit(9) --> "9".
+
+string_value(V) --> [V], { string(V) }.
+
+print_statement(print(V)) --> ["print"], ["("], value(V), [")"].
+
+% program(P, ["main", "{", a, "=", "Hello", ";","}"],[]).
+% program(P, ["main", "{", a, "=", 5.2, ";","}"],[]).
