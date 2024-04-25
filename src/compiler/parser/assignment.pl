80190fb2da9d2648d608960eb3e9f60529834cd6
Nisha Verma <nverma20@asu.edu>
2024-04-25 13:35:20 -0700

    Improvised identifier

--- src/compiler/parser/assignment.pl
+++ src/compiler/parser/assignment.pl
@@ -2,6 +2,7 @@
 % Nisha Verma; Parser for assignment; 1.0; 04/20/2024
 % Nisha Verma; Improvised assignment; 2.0; 04/25/2024
 % Nisha Verma; Improved parse tree for print statement; 3.0; 04/25/2024
+% Nisha Verma; Improvised identifier; 4.0; 04/25/2024
 :- use_rendering(svgtree).
 
 program(Parse) --> main_method(Parse).
@@ -10,7 +11,7 @@ main_method(main_block(Block)) --> ["main"], block(Block).
 block(block(Statements)) -->  ["{"], statement_list(Statements), ["}"].
 statement_list(sl(S,R)) --> statement(S),statement_list(R).
 statement_list(sl(S))--> statement(S).
-statement(print(V)) --> ["print"], ["("], value(V), [")"], [";"].
+statement(print(V)) --> ["print"], ["("], value(V), [")"], [";"].
 statement(assignment(Identifier, Value)) --> id(Identifier), ["="], value(Value), [";"].
 
 value(number(X)) --> [X], { number(X) }.
@@ -22,7 +23,8 @@ value(boolean('F')) --> [F], { member(F, ['F']) }.
 numeric_value(I) --> integer(I).
 numeric_value(F) --> float(F).
 
-id(identifier(X)) --> [X],{ member(X, [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]) }.
+id(identifier(X)) --> [X], {member(X, [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]) }.
+id(identifier(X)) --> [X], {atom(X), \+ member(X, [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]) }.
 
 integer(int(I)) --> digit_sequence(I).
 float(float(I,D)) --> digit_sequence(I), [“.”], digit_sequence(D).
@@ -42,5 +44,5 @@ digit(9) --> "9".
 string_value(V) --> [V], { string(V) }.
 
 % program(P, ["main", "{", a, "=", "Hello", ";","}"],[]).
-% program(P, ["main", "{", a, "=", 5.2, ";","}"],[]).
+% program(P, ["main", "{", temp, "=", 5.2, ";","}"],[]).
 % program(P, ["main", "{", "print", "(", "Hello", ")", ";","}"],[]).
